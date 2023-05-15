e113b1f4f8a76c690e307255e728bb78b337635d
lanlu <lanluuvan02@gmail.com>
2023-05-15 15:08:49 +0200

    DatabaseMasse

---
+++ app/src/main/java/com/example/EnergizeMe/myfirstapp/Datenbank/DataBaseMasse.java
@@ -0,0 +1,98 @@
+package com.example.EnergizeMe.myfirstapp.Datenbank;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseMasse extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "masse.db";
+    public static final String TABLE_NAME = "masse_table";
+
+    public static final String COL_1 = "MASSEID";
+    public static final String COL_2 = "GEBDATUM";
+    public static final String COL_3 = "GESCHLECHT";
+    public static final String COL_4 = "GROESSE";
+    public static final String COL_5 = "GEWICHT";
+    public static final String COL_6 = "ZIEL";
+    public static final String COL_7 = "TAETIGKEITSLEVEL";
+    public static final String COL_8 = "TAEGLICHEPUNKTE";
+    public static final String COL_9 = "PUNKTESTANDTAG";
+    public static final String COL_10 = "PUNKTESTANDWOCHE";
+
+    public static final String COL_11 = "BENUTZERID";
+
+    public DataBaseMasse(@Nullable Context context) {
+        super(context,DATABASE_NAME,null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (MASSEID INTEGER PRIMARY KEY AUTOINCREMENT, GEBDATUM TEXT, GESCHLECHT TEXT" +
+                ",GEWICHT INTEGER, ZIEL TEXT, TAETIGKEITSLEVEL INTEGER, TAEGLICHEPUNKTE INTEGER, PUNKTESTANDTAG INTEGER, PUNKTESTANDWOCHE INTEGER) ");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " +  TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData( String gebDatum,
+                              String geschlecht, String groesse,String gewicht, String ziel,
+                              String taetigkeitslevel, String taeglichePunkte, String punktestandtag, String punktestandwoche) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, gebDatum);
+        contentValues.put(COL_3, geschlecht);
+        contentValues.put(COL_4, groesse);
+        contentValues.put(COL_5, gewicht);
+        contentValues.put(COL_6, ziel);
+        contentValues.put(COL_7, taetigkeitslevel);
+        contentValues.put(COL_8, taeglichePunkte);
+        contentValues.put(COL_9, punktestandtag);
+        contentValues.put(COL_10, punktestandwoche);
+        // wenn db.insert -1 returned = Fehler, wenn ID der Zeile ausgegeben = hat geklappt
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+
+    public boolean updateData(String masseid, String gebDatum,
+                              String geschlecht, String groesse,String gewicht, String ziel,
+                              String taetigkeitslevel, String taeglichePunkte, String punktestandtag, String punktestandwoche) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, masseid);
+        contentValues.put(COL_2, gebDatum);
+        contentValues.put(COL_3, geschlecht);
+        contentValues.put(COL_4, groesse);
+        contentValues.put(COL_5, gewicht);
+        contentValues.put(COL_6, ziel);
+        contentValues.put(COL_7, taetigkeitslevel);
+        contentValues.put(COL_8, taeglichePunkte);
+        contentValues.put(COL_9, punktestandtag);
+        contentValues.put(COL_10, punktestandwoche);
+        db.update(TABLE_NAME, contentValues, "nutzerid = ?", new String[] { masseid });
+        return true;
+    }
+
+    public Integer deleteData (String masseid){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "nutzerid = ?", new String[] {masseid});
+    }
+}
