ea820f8601eeea5dafbf12da085f498c3b37f543
lanlu <lanluuvan02@gmail.com>
2023-05-15 15:08:29 +0200

    DatabaseMasse

--- app/src/main/java/com/example/EnergizeMe/myfirstapp/Datenbank/DataBaseHelperBenutzer.java
+++ app/src/main/java/com/example/EnergizeMe/myfirstapp/Datenbank/DataBaseHelperBenutzer.java
@@ -12,16 +12,9 @@ public class DataBaseHelperBenutzer extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "nutzer.db";
     public static final String TABLE_NAME = "nutzer_table";
     public static final String COL_1 = "NUTZERID";
-    public static final String COL_2 = "NAME";
-    public static final String COL_3 = "GEBDATUM";
-    public static final String COL_4 = "GESCHLECHT";
-    public static final String COL_5 = "GROESSE";
-    public static final String COL_6 = "GEWICHT";
-    public static final String COL_7 = "ZIEL";
-    public static final String COL_8 = "TAETIGKEITSLEVEL";
-    public static final String COL_9 = "TAEGLICHEPUNKTE";
-    public static final String COL_10 = "PUNKTESTANDTAG";
-    public static final String COL_11 = "PUNKTESTANDWOCHE";
+    public static final String COL_2 = "VORNAME";
+    public static final String COL_3 = "NACHNAME";
+
 
     public DataBaseHelperBenutzer(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -31,8 +24,7 @@ public class DataBaseHelperBenutzer extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, GEBDATUM TEXT, GESCHLECHT TEXT" +
-                ",GEWICHT INTEGER, ZIEL TEXT, TAETIGKEITSLEVEL INTEGER, TAEGLICHEPUNKTE INTEGER, PUNKTESTANDTAG INTEGER, PUNKTESTANDWOCHE INTEGER) ");
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, VORNAME TEXT, NACHNAME TEXT) ");
     }
 
     @Override
@@ -41,21 +33,12 @@ public class DataBaseHelperBenutzer extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertData(String name, String gebDatum,
-                              String geschlecht, String groesse,String gewicht, String ziel,
-                              String taetigkeitslevel, String taeglichePunkte, String punktestandtag, String punktestandwoche) {
+    public boolean insertData(String vorname, String nachname) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2, name);
-        contentValues.put(COL_3, gebDatum);
-        contentValues.put(COL_4, geschlecht);
-        contentValues.put(COL_5, groesse);
-        contentValues.put(COL_6, gewicht);
-        contentValues.put(COL_7, ziel);
-        contentValues.put(COL_8, taetigkeitslevel);
-        contentValues.put(COL_9, taeglichePunkte);
-        contentValues.put(COL_10, punktestandtag);
-        contentValues.put(COL_11, punktestandwoche);
+        contentValues.put(COL_2, vorname);
+        contentValues.put(COL_3, nachname);
+        
         // wenn db.insert -1 returned = Fehler, wenn ID der Zeile ausgegeben = hat geklappt
         long result = db.insert(TABLE_NAME, null, contentValues);
         if(result == -1) {
@@ -71,23 +54,14 @@ public class DataBaseHelperBenutzer extends SQLiteOpenHelper {
         return res;
     }
 
-    public boolean updateData(String id, String name, String gebDatum,
-                              String geschlecht, String groesse,String gewicht, String ziel,
-                              String taetigkeitslevel, String taeglichePunkte, String punktestandtag, String punktestandwoche) {
+    public boolean updateData(String masseid, String vorname, String nachname) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_1, id);
-        contentValues.put(COL_2, name);
-        contentValues.put(COL_3, gebDatum);
-        contentValues.put(COL_4, geschlecht);
-        contentValues.put(COL_5, groesse);
-        contentValues.put(COL_6, gewicht);
-        contentValues.put(COL_7, ziel);
-        contentValues.put(COL_8, taetigkeitslevel);
-        contentValues.put(COL_9, taeglichePunkte);
-        contentValues.put(COL_10, punktestandtag);
-        contentValues.put(COL_11, punktestandwoche);
-        db.update(TABLE_NAME, contentValues, "nutzerid = ?", new String[] { id });
+        contentValues.put(COL_1, masseid);
+        contentValues.put(COL_2, vorname);
+        contentValues.put(COL_3, nachname);
+        
+        db.update(TABLE_NAME, contentValues, "nutzerid = ?", new String[] { masseid });
         return true;
     }
 
