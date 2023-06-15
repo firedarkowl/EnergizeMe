25fac8c75e919b63894e616ea5647da90a8eb67b
waelm <wael.al-amri@haw-hamburg.de>
2023-06-15 14:43:50 +0200

    Version 2 mit letzte Backend Version und Tests

--- app/src/main/java/com/example/EnergizeMe/myfirstapp/Logik/Aktivitat.java
+++ app/src/main/java/com/example/EnergizeMe/myfirstapp/Logik/Aktivitat.java
@@ -5,6 +5,8 @@ import androidx.annotation.RequiresApi;
 import com.example.EnergizeMe.myfirstapp.Logik.Benutzer;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**Diese Klasse implemntiert den Aktivität von den Nutzer von Energize-Me App*/
 public class Aktivitat {
 
@@ -14,19 +16,35 @@ public class Aktivitat {
     private int dauer;
     private LocalDate lastEvent;
 
-    public Aktivitat(String sportArt,LocalDate lastEvent,int dauer){
+    public Aktivitat(String sportArt,LocalDate lastEvent,int dauer, Benutzer benutzer){
         this.sportArt=sportArt;
         this.lastEvent=lastEvent;
         this.dauer=dauer;
+        this.benutzer=benutzer;
+    }
+    //Die Methode speichert auch den Betrag der Punkte, die täglich gespart werden dürfen,
+    // und stellt sicher, dass diese Punkte nicht verfallen, solange sie innerhalb von 7 Tagen verwendet werden.
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public double berechneVerbrannteKalorien() {
+        double aktPunkte = 0.0;
+        if (sportArt != null &&sportArt.equals("Leicht Sport")) {
+            //wtf ist das für eine hart gecodete Zahl?!
+            aktPunkte += (dauer * benutzer.getWeight() / 1940.0);
+        } else if (sportArt != null &&sportArt.equals("Mittel Intensiv")) {
+            aktPunkte += (dauer * benutzer.getWeight()) / 1400.0;
+        } else if (sportArt != null &&sportArt.equals("Intensiv Sport")) {
+            aktPunkte += (dauer * benutzer.getWeight()) / 560.0;
+        } else {
+            throw new IllegalArgumentException("Unbekannte Sportart: " + sportArt);
+        }
+        benutzer.taglichePunktstand();
+        return aktPunkte;
     }
 
     public Benutzer getBenutzer() {
         return benutzer;
     }
 
-    public void setBenutzer(Benutzer benutzer) {
-        this.benutzer = benutzer;
-    }
 
     public String getSportArt() {
         return sportArt;
@@ -60,35 +78,7 @@ public class Aktivitat {
         this.lastEvent = lastEvent;
     }
 
-    //Die Methode speichert auch den Betrag der Punkte, die täglich gespart werden dürfen,
-    // und stellt sicher, dass diese Punkte nicht verfallen, solange sie innerhalb von 7 Tagen verwendet werden.
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public double berechneVerbrannteKalorien() {
-        double punkte=0.0;
-        if (sportArt.equals("Leicht Sport")) {
-            //wtf ist das für eine hart gecodete Zahl?!
-            punkte += (dauer * benutzer.getWeight() /1940.0);
-        } else if (sportArt.equals("Mittel Intensiv")) {
-            punkte += (dauer * benutzer.getWeight()) / 1400.0;
-        } else if (sportArt.equals("Intensiv Sport")) {
-            punkte += (dauer * benutzer.getWeight()) / 560.0;
-        } else {
-            throw new IllegalArgumentException("Unbekannte Sportart: " + sportArt);
-        }
 
-        // Wenn es bereits ein Event gab und eine Woche seitdem vergangen ist, setzen Sie gesparte Punkte auf 0
-        LocalDate today = LocalDate.now();
-        if (today.isAfter(lastEvent.plusWeeks(1))) {
-            savedPoints = 0;
-        }
 
-        savedPoints +=punkte;
-        //Hier wird die maximale Anzahl von Punkten berechnet, die täglich gespart werden können.
-        // Dabei wird die aktuelle Anzahl der gespeicherten Punkte savedPoints mit dem Wert 4.0 verglichen und der kleinere Wert ausgewählt.
-        double dailySavedPoints = Math.min(savedPoints, 4.0);
-        //Die Anzahl der gespeicherten Punkte wird um die Anzahl der täglich gesparten Punkte reduziert.
-        savedPoints -= dailySavedPoints;
-        // Ziehe die Punkte ab, was man isst von sportpunkte, sorge dafür, dass die Punktzahl nicht negativ wird
-        return Math.max(benutzer.getPunktstandwoche() - dailySavedPoints, 0.0);
-    }
 }
+
